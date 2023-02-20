815a17c45b9a1a93082a47d08c54bddf54f8dd23
Marc Cuesta <marc.cuestacarabasa@gmail.com>
2023-02-20 17:53:07 +0100

    Bidirectional binding

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -11,10 +11,24 @@ export class HelloComponent{
   @Input() name:string;
   @Output() sayHello:EventEmitter<string> = new EventEmitter<string>();
 
+  public namesList: Array<string> = [];
+  public inputName:string = "John";
+
   constructor(){
   }
 
   onNameClick() {
     this.sayHello.emit("Hello!");
+  }
+
+  addName(){
+    this.namesList.push(this.inputName);
+    this.inputName = "";
+    console.log(this.namesList);
+  }
+
+  updateInputName(event:any){
+    this.inputName = event.target.value;
+    console.log(this.inputName);
   }
 }
