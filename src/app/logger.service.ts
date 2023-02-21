0671cac0c24624fb5e9ebb280ccb57dfcbd5510a
Marc Cuesta <marc.cuestacarabasa@gmail.com>
2023-02-21 19:44:31 +0100

    Services tree shaking

--- src/app/logger.service.ts
+++ src/app/logger.service.ts
@@ -1,3 +1,11 @@
+import { Injectable } from "@angular/core";
+
+/* Decoramos el servicio logger con injectable, diciendo a angular que lo meta en el inyector del 
+component principal root, para que así cuando instanciemos logger el navegador lo cargue, y mientras
+no se instancie no (tree-shaking).  */
+@Injectable({
+  providedIn: "root"
+})
 export class LoggerService{
   log(msg:string){ console.log(msg); }
   error(msg:string){ console.error(msg); }
